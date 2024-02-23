f05c9e5ad0a27542ae8c07663751c673fd340cd7
simon <72697077+simondidxyz@users.noreply.github.com>
2024-02-23 08:41:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/oamovdb',
   plugins: [react(), eslint()],
   resolve: {
     alias: {
